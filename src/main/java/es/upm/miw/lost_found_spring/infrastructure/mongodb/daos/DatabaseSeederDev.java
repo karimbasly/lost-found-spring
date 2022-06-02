c370ef0ca0643c7fbd25482169cb0cac0000fcf1
karimbasly <karimbasli@live.fr>
2022-06-02 22:32:25 +0200

    ADD Test

--- src/main/java/es/upm/miw/lost_found_spring/infrastructure/mongodb/daos/DatabaseSeederDev.java
+++ src/main/java/es/upm/miw/lost_found_spring/infrastructure/mongodb/daos/DatabaseSeederDev.java
@@ -1,7 +1,11 @@
 package es.upm.miw.lost_found_spring.infrastructure.mongodb.daos;
 
+import es.upm.miw.lost_found_spring.domain.model.Category;
+import es.upm.miw.lost_found_spring.domain.model.Type;
 import es.upm.miw.lost_found_spring.infrastructure.api.http_errors.Role;
+import es.upm.miw.lost_found_spring.infrastructure.mongodb.daos.synchronous.AnnouncementDao;
 import es.upm.miw.lost_found_spring.infrastructure.mongodb.daos.synchronous.UserDao;
+import es.upm.miw.lost_found_spring.infrastructure.mongodb.entities.AnnouncementEntity;
 import es.upm.miw.lost_found_spring.infrastructure.mongodb.entities.UserEntity;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +21,13 @@ public class DatabaseSeederDev {
 
     private final DatabaseStarting databaseStarting;
     private final UserDao userDao;
+    private final AnnouncementDao announcementDao;
 
     @Autowired
-    public DatabaseSeederDev(DatabaseStarting databaseStarting, UserDao userDao) {
+    public DatabaseSeederDev(DatabaseStarting databaseStarting, UserDao userDao, AnnouncementDao announcementDao) {
         this.databaseStarting = databaseStarting;
         this.userDao = userDao;
+        this.announcementDao = announcementDao;
         this.deleteAllAndInitializeAndSeedDataBase();
 
     }
@@ -34,7 +40,8 @@ public class DatabaseSeederDev {
 
     private void deleteAllAndInitialize() {
         LogManager.getLogger(this.getClass()).warn("------- Delete All -------");
-        //this.userDao.deleteAll();
+        this.userDao.deleteAll();
+        this.announcementDao.deleteAll();
         this.databaseStarting.initialize();
     }
 
@@ -50,6 +57,17 @@ public class DatabaseSeederDev {
         this.userDao.saveAll(List.of(users));
         //this.userDao.deleteAll(List.of(users));
         LogManager.getLogger(this.getClass()).warn("        ------- users");
+        UserEntity userEntity = UserEntity.builder().id("id35").userName("test").email("test@a.a").familyName("Name1")
+                .mobile(12365).location("Spain")
+                .password(pass)
+                .role(Role.CUSTOMER).registrationDate(LocalDateTime.now()).build();
+        AnnouncementEntity[] announcementEntities = {
+                AnnouncementEntity.builder().id("id1").category(Category.PETS).description("des").location("tunis")
+                        .name("Cat").photo("").type(Type.LOST).userEntity(userEntity)
+                        .build(),
+        };
+        this.announcementDao.saveAll(List.of(announcementEntities));
+        LogManager.getLogger(this.getClass()).warn("        ------- AnnouncementEntity");
     }
 
 }
